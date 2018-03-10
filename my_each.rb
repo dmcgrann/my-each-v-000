0aef1702ecc48223596fa583977a07eff845fdf2
David McGrann <dpmcgrann@gmail.com>
2018-03-10 14:46:49 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  new_array = []
+  counter = 0
+  while e > 0
+    yield
+    counter += 1
+  end
 end
+
+array.each do |e|
+  new_array << e
