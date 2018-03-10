2df8457bcd424320b23d4a09e2879ac133354e0e
David McGrann <dpmcgrann@gmail.com>
2018-03-10 14:51:26 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,8 @@
 def my_each(array)
-  new_array = []
-  counter = 0
-  while e > 0
-    yield
+  if block_given?
+  i = 0
+  while i > 0
+    yield(array[i])
     counter += 1
   end
-  array.map {|e| new_array << e }
 end
